17fa7d012d5fc32f9b89c968e2f2cc4bcf2cdd69
Jorge Calderita <jcalderita@outlook.com>
2024-09-12 19:17:57 +0200

    config: Add options to deploy
    - Add deploy to cloudflare yml
    - Change name of project for CloudFare project name
    - Change name of package

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "IgniteStarter",
+    name: "JCalderitaWebSite",
     platforms: [.macOS(.v13)],
     dependencies: [
         .package(url: "https://github.com/twostraws/Ignite.git", branch: "main")
     ],
     targets: [
         .executableTarget(
-            name: "IgniteStarter",
+            name: "JCalderitaWebSite",
             dependencies: ["Ignite"]),
     ]
 )
