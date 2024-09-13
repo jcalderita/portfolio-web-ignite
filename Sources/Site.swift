150bc2b7ece40b1d122635890ece38ec0410a0fa
Jorge Calderita <jcalderita@outlook.com>
2024-09-13 14:15:03 +0200

    config: Add favicon

--- Sources/Site.swift
+++ Sources/Site.swift
@@ -20,8 +20,10 @@ struct JcalderitaSite: Site {
     var url = URL("https://www.jcalderita.com")
     var builtInIconsEnabled = true
     var pageWidth = 12
-
+    var favicon = URL("/images/favicon.png")
+    var feedConfiguration = FeedConfiguration(mode: .full, contentCount: 20, image: .init(url: "https://www.jcalderita.com/images/favicon.png", width: 32, height: 32))
     var author = "Jorge Calderita"
+
 
     var homePage = Home()
     var theme = MyTheme()
